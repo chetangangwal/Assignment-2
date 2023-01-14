2acec85d027578277c5faa79c6790b1660ae5999
chetangangwal <64461545+chetangangwal@users.noreply.github.com>
2023-01-14 14:19:17 +0530

    Add files via upload

---
+++ Assignment2ans2.py
@@ -0,0 +1,8 @@
+name = str("chetan gangwal")
+sid = int("22106006")
+department_name = str("CSE DS")
+cgpa = float("9.9")
+
+print("Hey,", name, "here!")
+print("My SID is", sid)
+print("I am from", department_name, "department and my cgpa is", cgpa)
